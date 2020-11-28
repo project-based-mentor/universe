b40f86abdcca6287d3f1f1d7a1fe599a65ed9081
Thomas Farla <thomasfarla15+github@gmail.com>
2020-11-28 01:26:04 +0100

    Make stories visible in storybook and update site

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,21 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-    "../apps/*/frontend/src/stories/*.stories.mdx",
-    "../apps/*/frontend/src/stories/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+    "stories": [
+        "../stories/**/*.stories.mdx",
+        "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+        "../apps/*/frontend/src/stories/*.stories.mdx",
+        "../apps/*/frontend/src/stories/*.stories.@(js|jsx|ts|tsx)"
+    ],
+    "addons": [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/addon-storysource",
+        {
+            name: "@storybook/addon-docs",
+            options: {
+                sourceLoaderOptions: {
+                    injectStoryParameters: false,
+                },
+            },
+        },
+    ]
 }
